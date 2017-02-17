49a4d59b9f8ec10ef8d87cba223da5d56f90ed7c
Jonathan Lancaster <jcast90@me.com>
2017-02-16 17:45:46 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,134 @@
+//step 1  
+
+//var someMonth;
+//function theMonth 		// function to return current month
+//currentMonth 		// a constant
+//var summerMonth; 		// an array of summer months
+//function myLibraryAfunction 	// a function 
+
+
+//Step 2
+//var numeric = 40;
+//var string = 'this is a string';
+//var boolean = true;
+//var n = null; 
+
+//Step 3
+
+
+//4
+//var sFirstName;
+//var sLastName;
+//var sAddress;
+//var sCity;
+//var sState;
+//var sZipCode;
+//var sYourAge;
+//var sReferralSource; 
+//var bMayWeContactYou; 
+
+//step 5
+//sring example
+//var sFirstName = 'Jonathan';
+//var sLastName = 'Lancaster';
+//var sAddress = '3227 Mercer Lane';
+//var sCity = 'San Diego';
+//var sState = 'California';
+//numerical example
+//var sZipCode = 92122;
+//var sYourAge = 26;
+//var sReferralSource = 'school';
+//boolean example
+//var bMayWeContactYou = true; 
+
+//step 6
+//var x = 'I am ' + 26;
+//console.log(x);
+//var y = true + 'boolean example with string';
+//console.log(y);
+//var z = false + 26;
+//console.log(z);
+
+//step 7
+//console.log(y);
+//var y = 8;
+
+//step 8
+//var someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+//console.log(someString);
+
+//step 9
+//var x = null;
+//console.log(x,y);
+//var y = 9;
+
+//step 10
+//typeof 20 === 'number';
+//typeof 'string' === 'string';
+//typeof true === 'boolean';
+//typeof [1,2,3] === 'object';
+//typeof document.all === 'undefined';
+
+//step 11 
+//alert('Hello Jonathan, welcome to the JavaScript class');
+
+//step 12
+//var name = ' Jonathan.';
+
+//step 13
+//var course = 'JavaScript';
+
+//step 14
+//alert('Hello' + name + '\nWelcome to the ' + course + ' class');
+
+//step 15
+//var name = 'What is your name?';
+//alert(name);
+
+//step 16
+//var course = 'What course are you taking?';
+//alert(course);
+
+//step 17
+//var x = 10;
+//var y = 20;
+//console.log(x + y);
+
+//step 18
+//var x = 20;
+//x = x + 20;
+//console.log(x);
+
+//step 19
+//var x = 20;
+//x = x * 5;
+//console.log(x);
+
+//step 20
+//var x = 20 % 3;
+//x = x / 1;
+//console.log(x);
+
+//step 21
+//var x = 6;
+//var y = x != 3;
+//console.log(y);
+
+//step 22
+//var x = 6;
+//var y = x > 8;
+//console.log(y);
+
+//step 23
+//var widget = new Object();
+//console.log(typeof widget);
+
+//step 24
+//var widget = new Object();
+//console.log(widget instanceof Object);
+
+//step 25
+//var x;
+//var widget = new Object();
+//console.log(x instanceof Object);
+
